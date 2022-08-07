dbf87e143836f8b665b2aebe6d328e78483abd0d
jdvega21 <jdvega21@hotmail.com>
2022-08-07 17:16:56 -0600

    Products added to Index view dinamicly via JS

--- main.js
+++ main.js
@@ -4,7 +4,41 @@ const btnHambMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const btnShoppingCart = document.querySelector('.navbar-shopping-cart');
 const productDetailView = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
+//Default Items
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: '120,00',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'PC',
+    price: '500,00',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'T-shirt',
+    price: '10,00',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+/*<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" class="product-img">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+    
+</div> */
+       
 //Listener const funtions
 const toggleDesktopMenu = () => {
     const isProductDetailViewClosed = productDetailView.classList.contains('inactive');
@@ -32,8 +66,49 @@ const toggleProductDetailView = () => {
 
 };
 
+function renderProducts(arr){
+    for(product of arr){
+        const productCart = document.createElement('div');
+        productCart.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoChild = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + " " + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoChild.appendChild(productPrice);
+        productInfoChild.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const addToCartImg = document.createElement('img');
+        addToCartImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(addToCartImg);
+    
+        productInfo.appendChild(productInfoChild);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCart.appendChild(productImg);
+        productCart.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCart);
+    }
+    
+}
+
 /*Adding listeners above to HTML content*/
 emailMenu.addEventListener('click', toggleDesktopMenu);
 btnHambMenu.addEventListener('click', toggleMobilepMenu);
 btnShoppingCart.addEventListener('click', toggleProductDetailView);
+
+//Initializing
+renderProducts(productList);
 
