c153b8aea836dd383542aec040c071b2454aa120
jdvega21 <jdvega21@hotmail.com>
2022-08-07 15:00:26 -0600

    Mobile menu added with hamburger button

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnHambMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 //Listener const funtions
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle("inactive");
-  };
-  
+};
+const toggleMobilepMenu = () => {
+   mobileMenu.classList.toggle("inactive");
+};
 
+/*Adding listeners above to HTML content*/
 emailMenu.addEventListener('click', toggleDesktopMenu);
+btnHambMenu.addEventListener('click', toggleMobilepMenu);
 
